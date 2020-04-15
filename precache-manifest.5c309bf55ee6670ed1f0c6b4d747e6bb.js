8a68994f0d5f0e02d0feb54dba907ce2de6356ea
Bruce Dickson <brucearruk@gmail.com>
2020-04-15 14:34:26 -0300

    Updates

---
+++ precache-manifest.5c309bf55ee6670ed1f0c6b4d747e6bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8bd19d2744129d8cbda3056271e169d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0463f4d7a57cf891b97d",
+    "url": "/monsters-rolodex/static/css/main.e86463b0.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0463f4d7a57cf891b97d",
+    "url": "/monsters-rolodex/static/js/main.c406a405.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
